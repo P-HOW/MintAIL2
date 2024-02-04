5603ae8b1898ad0eebe1787811f3dd85a7437e05
P-How <peihaoli1014@gmail.com>
2024-02-04 10:42:18 +0100

    stake contract to be inherited by node contract

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,60 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.5.16;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "./interfaces/IERC20.sol";
 
 contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+    IERC20 public token;
+    uint public unlockTime; // Duration in seconds
+    address public owner;
 
-    event Withdrawal(uint amount, uint when);
+    struct Deposit {
+        uint amount;
+        uint unlockTime; // Specific unlock time for each deposit
+    }
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    mapping(address => Deposit) public deposits;
 
+    event DepositMade(address indexed depositor, uint amount, uint unlockTime);
+    event Withdrawal(address indexed withdrawer, uint amount);
+
+    constructor(IERC20 _token, address _owner, uint _unlockTime) public {
+        token = _token;
+        owner = _owner;
         unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    }
+
+    // Set unlock time in seconds (e.g., 1 day = 86400 seconds)
+    function setUnlockTime(uint _durationInSeconds) public {
+        require(msg.sender == owner, "Only owner can set unlock time");
+        unlockTime = _durationInSeconds;
+    }
+
+    function deposit(uint _amount) public {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(token.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+
+        uint depositUnlockTime = block.timestamp + unlockTime;
+
+        if (deposits[msg.sender].amount > 0) {
+            deposits[msg.sender].amount += _amount;
+            deposits[msg.sender].unlockTime = depositUnlockTime;
+        } else {
+            deposits[msg.sender] = Deposit(_amount, depositUnlockTime);
+        }
+
+        emit DepositMade(msg.sender, _amount, depositUnlockTime);
     }
 
     function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        require(block.timestamp >= deposits[msg.sender].unlockTime, "You can't withdraw yet");
+        require(deposits[msg.sender].amount > 0, "No deposit to withdraw");
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        uint amount = deposits[msg.sender].amount;
+        deposits[msg.sender].amount = 0;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+        require(token.transfer(msg.sender, amount), "Transfer failed");
 
-        owner.transfer(address(this).balance);
+        emit Withdrawal(msg.sender, amount);
     }
 }
+
