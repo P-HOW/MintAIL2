4fa36eada97dd657d0957276d4d92042e490358a
P-How <peihaoli1014@gmail.com>
2024-02-04 16:58:51 +0100

    added lock initialize

--- contracts/BaseCoordinator.sol
+++ contracts/BaseCoordinator.sol
@@ -9,8 +9,9 @@ import "./Lock.sol";
 
 contract BaseCoordinator is Lock, Owned, MixinResolver {
 
-    bytes32 private constant CONTRACT_MAI = "MAI";
-    bytes32 private constant CONTRACT_LOCK = "LockCoordinatorsV1";
+    bytes32 public constant CONTRACT_MAI_PROXY = "MAI_PROXY";
+    bytes32 public constant CONTRACT_LOCK = "LockCoordinatorsV1";
+    bool public  staking_initialized;
 
     struct CoordinatorInfo {
         string name; // Change to string
@@ -30,28 +31,33 @@ contract BaseCoordinator is Lock, Owned, MixinResolver {
     uint256 public percentageGettingElected;
 
     constructor(
-        IERC20 _token,
         address _lockOwner,
         uint _unlockTime,
         address _owner,
         address _resolver
     )
-    Lock(_token, _lockOwner, _unlockTime) // Initialize Lock
+    Lock( _lockOwner, _unlockTime) // Initialize Lock
     Owned(_owner) // Initialize Owned
     MixinResolver(_resolver) // Initialize MixinResolver
     public
     {
-        // Additional initialization (if any)
+        staking_initialized = false;
+    }
+
+    function _initialize() public onlyOwner {
+        require(!staking_initialized, "Already initialized");
+        token = maiTokenContract();
+        staking_initialized = true;
     }
 
     function resolverAddressesRequired() public view returns (bytes32[] memory addresses) {
         addresses = new bytes32[](1);
-        addresses[0] = CONTRACT_MAI;
+        addresses[0] = CONTRACT_MAI_PROXY;
         addresses[1] = CONTRACT_LOCK;
     }
 
     function maiTokenContract() internal view returns (IERC20) {
-        return IERC20(requireAndGetAddress(CONTRACT_MAI));
+        return IERC20(requireAndGetAddress(CONTRACT_MAI_PROXY));
     }
 
     function coordinatorLockContract() internal view returns (ILock) {
